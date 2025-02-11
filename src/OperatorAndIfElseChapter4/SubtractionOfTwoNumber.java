05fdf9af5c450524bf58c1c21e8428af4e1a91b9
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-11 23:48:40 +0530

    Another Commit

--- src/OperatorAndIfElseChapter4/SubtractionOfTwoNumber.java
+++ src/OperatorAndIfElseChapter4/SubtractionOfTwoNumber.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class SubtractionOfTwoNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("This is a simple a example of addition of two numbers");
+        System.out.println("This is a simple a example of subtraction of two numbers");
         System.out.print("\nEnter your first number: ");
         double a = input.nextDouble();
         System.out.print("Enter your second number: ");
         double b = input.nextDouble();
         double c = a - b;
-        System.out.println("The addition of the given number is " + c);
+        System.out.println("The subtraction of the given number is " + c);
     }
 }
