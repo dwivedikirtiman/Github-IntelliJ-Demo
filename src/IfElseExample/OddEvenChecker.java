08ad5075144ea5a2f1525dd87b2a30bef1d97b0e
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-12 23:37:40 +0530

    Anther Commit

--- src/IfElseExample/OddEvenChecker.java
+++ src/IfElseExample/OddEvenChecker.java
@@ -1,5 +1,28 @@
 package IfElseExample;
 
+import java.util.Scanner;
+
 public class OddEvenChecker {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("This is a simple odd even checker program");
+        System.out.print("\nEnter your number to check: ");
+        /*int  number = input.nextInt();
+        if((number/2) == (int)(number/2)){
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }*/
+        //method 1 is this and method 2 is as follows:
+
+        int number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+
+
+        }
+    }
 }
