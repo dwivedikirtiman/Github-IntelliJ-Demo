c6e97efc9fc699ddbacb164046ff3e298f7c910e
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-12 23:41:33 +0530

    Anther Commit

--- src/IfElseExample/OddEvenChecker.java
+++ src/IfElseExample/OddEvenChecker.java
@@ -16,13 +16,24 @@ public class OddEvenChecker {
         }*/
         //method 1 is this and method 2 is as follows:
 
-        int number = input.nextInt();
+        /*int number = input.nextInt();
         if (number % 2 == 0) {
             System.out.println("The number is even");
         } else {
             System.out.println("The number is odd");
+        }*/
 
+        //Another Method
 
+        if (input.hasNextInt()) {  // Ensures the input is an integer
+            int num = input.nextInt();
+            if ((num / 2) * 2 == num) { // Checking divisibility by 2
+                System.out.println(num + " is Even.");
+            } else {
+                System.out.println(num + " is Odd.");
+            }
+        } else {
+            System.out.println("Invalid input! Please enter an integer.");
         }
     }
 }
