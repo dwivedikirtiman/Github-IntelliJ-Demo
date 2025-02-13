ad56552e7deffe95df29929be3229c4451a98e0c
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-14 00:42:47 +0530

    Another Commit

--- src/IfElseExample/AgeCheckerProgram.java
+++ src/IfElseExample/AgeCheckerProgram.java
@@ -9,12 +9,24 @@ public class AgeCheckerProgram {
         System.out.print("\nEnter you Age here: ");
         int age = input.nextInt();
 
-        if(age >= 60){
+       /* if(age >= 60){
             System.out.println("You are a senior Citizen");
-        } else if (age >= 21 && age < 60) {
+        } else if (age >= 20 && age < 60) {
             System.out.println("you are an adult");
-        } else if (age >13 && age <=20) {
+        } else if (age >13 && age <20) {
             System.out.println("you are a teen");
+        }else{
+            System.out.println("you are a child");
+        }*/
+
+        //Alternate way to write the same
+
+        if(age >= 65){
+            System.out.println("You are a senior Citizen");
+        } else if (age >= 20) {
+            System.out.println("you are an adult");
+        } else if (age >= 13) {
+            System.out.println("you are a teenager");
         }else{
             System.out.println("you are a child");
         }
