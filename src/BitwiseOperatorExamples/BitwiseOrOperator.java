a3aa5b528026c56e394a19861b85f06d9f7b6f42
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-14 23:30:10 +0530

    Another Commit

--- src/BitwiseOperatorExamples/BitwiseOrOperator.java
+++ src/BitwiseOperatorExamples/BitwiseOrOperator.java
@@ -2,16 +2,16 @@ package BitwiseOperatorExamples;
 
 import java.util.Scanner;
 
-public class BitwiseAndOperator {
+public class BitwiseOrOperator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("This program will show Bitwise Add operator");
+        System.out.println("This program will show Bitwise OR operator");
         System.out.print("\nEnter your first number: ");
         int first = input.nextInt();
         System.out.print("\nEnter your second number: ");
         int second = input.nextInt();
 
-        int result = first & second;
-        System.out.println("The Bitwise And operation for the given number is " + result);
+        int result = first | second;
+        System.out.println("The Bitwise OR operation for the given number is " + result);
     }
 }
