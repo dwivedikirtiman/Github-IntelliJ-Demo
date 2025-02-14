bbeeba3b00dd499205a3570cc46b864cbbdee805
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-15 00:01:02 +0530

    Another Commit

---
+++ src/BitwiseOperatorExamples/BitwiseRightShiftOperator.java
@@ -0,0 +1,16 @@
+package BitwiseOperatorExamples;
+
+import java.util.Scanner;
+
+public class BitwiseRightShiftOperator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("This program will show Bitwise Right Shift operator");
+        System.out.print("\nEnter your number: ");
+        int first = input.nextInt();
+
+        int result = first >> 1;
+        System.out.println("The Bitwise Right Shift operation for the given number is " + result);
+    }
+}
+
