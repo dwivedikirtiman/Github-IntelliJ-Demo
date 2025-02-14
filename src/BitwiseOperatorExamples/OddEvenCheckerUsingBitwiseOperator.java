5de8f115c516e419ef200a12c3a4884c940f64eb
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-15 00:14:06 +0530

    Another Commit

---
+++ src/BitwiseOperatorExamples/OddEvenCheckerUsingBitwiseOperator.java
@@ -0,0 +1,18 @@
+package BitwiseOperatorExamples;
+
+import java.util.Scanner;
+
+public class OddEvenCheckerUsingBitwiseOperator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("This program will check using Bitwise Operator");
+        System.out.print("\nEnter your number: ");
+        int number = input.nextInt();
+
+        if((number & 1) == 1){
+            System.out.println("The number is odd");
+        }else{
+            System.out.println("The number is even");
+        }
+    }
+}
