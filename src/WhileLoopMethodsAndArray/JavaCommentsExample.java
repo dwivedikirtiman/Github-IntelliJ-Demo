13348558f037941dcf5c249421925cbf69cf2760
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-15 23:58:59 +0530

    Another Commit

---
+++ src/WhileLoopMethodsAndArray/JavaCommentsExample.java
@@ -0,0 +1,10 @@
+package WhileLoopMethodsAndArray;
+
+public class JavaCommentsExample {
+    public static void main(String[] args) {
+        System.out.println("This program is just for showing comments type in java:");
+        System.out.println("This is \\ single line of comment");
+        System.out.println("This is /* */ Multiline comment");
+        System.out.println("This is /** */ Docs line of comment use for creating a document in java ");
+    }
+}
