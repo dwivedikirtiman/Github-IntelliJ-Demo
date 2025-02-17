9413828a6b73d73c4777b491843ad76b642a733b
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-17 20:32:29 +0530

    Another Commit

--- src/WhileLoopMethodsAndArray/MultiplicationTableOfGivenNumber.java
+++ src/WhileLoopMethodsAndArray/MultiplicationTableOfGivenNumber.java
@@ -8,11 +8,23 @@ public class MultiplicationTableOfGivenNumber {
         System.out.println("This program will print the multiplication table of the given number");
         System.out.print("\nEnter your number: ");
         int num = input.nextInt();
+        printMultiplicationOfTheNumber(num);
 
-        int i =1;
+//        int i =1;
+//        while(i <= 10){
+//            System.out.println(num + " X " + i + " = " + (num * i));
+//            i++;
+
+        //The same thing we can do through the help of method/function as follows
+        }
+
+    public static void printMultiplicationOfTheNumber(int num){
+        int i = 1;
         while(i <= 10){
-            System.out.println(num + " X " + i + " = " + (num * i));
+            System.out.println(num + "X" + i + " = " + (num * i));
             i++;
         }
+
     }
+
 }
