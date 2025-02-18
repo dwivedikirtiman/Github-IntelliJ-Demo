64760707dc6d0447ce87f7ea7d5453dc52bb5c9a
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-18 23:27:09 +0530

    Another Commit

--- src/WhileLoopMethodsAndArray/PrimeNumberChecker.java
+++ src/WhileLoopMethodsAndArray/PrimeNumberChecker.java
@@ -3,29 +3,39 @@ package WhileLoopMethodsAndArray;
 import java.util.Scanner;
 
 public class PrimeNumberChecker {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("This program will check whether the given number is prime or not");
         System.out.print("\nEnter your number: ");
         int num = input.nextInt();
+        input.close();
 
         boolean isPrime = isPrime(num);
-        if(isPrime){
+        if (isPrime) {
             System.out.println("Your number is prime");
-        }else{
+        } else {
             System.out.println("Your number is not prime");
         }
     }
 
-    public static boolean isPrime(int num){
-        int i =2;
-        while(i < num){
-            if(num % i == 0){
+    public static boolean isPrime(int num) {
+        if (num <= 1) {  // Handle edge cases: 0, 1, and negative numbers
+            return false;
+        }
+        if (num == 2) {  // 2 is the only even prime number
+            return true;
+        }
+        if (num % 2 == 0) {  // Exclude even numbers greater than 2
+            return false;
+        }
+
+        int i = 3;
+        while (i * i <= num) {  // Check divisibility only up to sqrt(num)
+            if (num % i == 0) {
                 return false;
             }
-            i++;
+            i += 2;  // Skip even numbers
         }
         return true;
-
     }
 }
---
+++ src/WhileLoopMethodsAndArray/ReverseTheDigit.java
@@ -0,0 +1,30 @@
+package WhileLoopMethodsAndArray;
+
+import java.util.Scanner;
+
+public class ReverseTheDigit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("This  program will reverse the digit of the given number");
+        System.out.print("\nEnter the  number: ");
+        int num = input.nextInt();
+        input.close();
+
+
+        int reverseNum = reverseNum(num);
+        System.out.println("The reverse of this number is : " + reverseNum);
+    }
+
+    public static int reverseNum(int num){
+        int newNum=0;
+        boolean isNegative = num < 0; // Check if the number is negative
+        num = Math.abs(num);
+        while(num > 0){
+            int digit = num % 10;
+            newNum = newNum * 10 + digit;
+            num = num/10;
+
+        }
+        return isNegative ? -newNum : newNum; // Restore negative sign if needed
+    }
+}
