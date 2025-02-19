663d21b9fbbb1b056d2eb2aeef20d0ffb9a8155c
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-19 22:26:28 +0530

    Another Commit

--- src/WhileLoopMethodsAndArray/PrintNumberFrom1toN.java
+++ src/WhileLoopMethodsAndArray/PrintNumberFrom1toN.java
@@ -9,12 +9,25 @@ public class PrintNumberFrom1toN {
         System.out.print("\nEnter a number: ");
         int num = input.nextInt();
 
-        /*int i =1;
+    /*  int i =1;
+        System.out.println("The number till the given number are: ");
         while(i <= num){
             System.out.print(i + " ");
             i++;
         }*/
 
         //if we don't want to write in main method directly then we can create the method as follows
+
+        System.out.print("The numbers till the given number are: ");
+        printNum(num);
+    }
+
+    public static void printNum(int num){
+        int i =1;
+        while(i <= num){
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
     }
 }
