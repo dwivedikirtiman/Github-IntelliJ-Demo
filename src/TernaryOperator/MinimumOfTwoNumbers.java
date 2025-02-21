3e789066015a3e4deb8dbc699777db7441696f52
Kirtiman Dwivedi <dwivedikirtiman00@gmail.com>
2025-02-22 00:30:25 +0530

    Another Commit

--- src/TernaryOperator/MinimumOfTwoNumbers.java
+++ src/TernaryOperator/MinimumOfTwoNumbers.java
@@ -19,7 +19,24 @@ public class MinimumOfTwoNumbers {
 
         //the above lines  of if else block can be removed through ternary operator as follows-
 
-        int minResult = num1 < num2 ? num1 : num2;
+       /* int minResult = num1 < num2 ? num1 : num2;
+        System.out.println("The minimum number is " + minResult);*/
+
+        //as we have used ternary operator above but we can and we should always put all this in a method so
+        //this all can be written under a method  as follows:
+
+        MinimumOfTwoNumbers minTwo = new MinimumOfTwoNumbers();  //object creation of the class to access teh method as method is not static
+        int minResult = minTwo.minMethod(num1, num2);  //agar mera method static declare rehta to ye error nahi deta but kyuki ye
+                                                    // static nahi declare hai to abhi mujhe apne class ka object banana padega and then through that object i can access the method which i've created
         System.out.println("The minimum number is " + minResult);
+    }
+
+    public int minMethod(int num1, int num2){
+        return num1 < num2 ? num1 : num2;
+       /* if(num1 < num2){
+            return num1;
+        }else{
+            return num2;
+        }*/
     }
 }
